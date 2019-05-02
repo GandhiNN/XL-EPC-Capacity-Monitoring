c2e898f1442c5f9b2266676fb8508e28af3d584b
Ngakan Nyoman Gandhi <n.nyoman.gandhi@gmail.com>
2019-05-02 15:11:09 +0700

    add functions to construct the final table

--- packages/throughput_neater.py
+++ packages/throughput_neater.py
@@ -147,14 +147,13 @@ def TransformMSD(df, column_name, coefficient=3):
     df_msd = df[np.abs(df[column_name]-df[column_name].mean()) <= (coefficient * df[column_name].std())]
     return df_msd
 
-###########
-# CleanThroughputDataAndVisualize is a function to do
-# all cleaning functions in one swoop. It accepts
-# an excel file and will load the content into
-# dataframe. It will visualize all the data for visual checks
+
+# CleanThroughputDataAndVisualize is a function to do all cleaning functions in one swoop.
+# It accepts an excel file and will load the content into dataframe. 
+# It will visualize all the data for visual checks
 def CleanThroughputDataAndVisualize(file_excel):
     # Load the excel file
-    df = LoadDFThroughput(file_excel)
+    df = LoadDFThroughputFromExcel(file_excel)
     # Visualize the original graph for all nodes
     VisualizeTimeSeriesThroughput(df)
     # Loop through each unique node name
@@ -181,6 +180,61 @@ def CleanThroughputDataAndVisualize(file_excel):
         df_node = TransformMSD(df_node, 'Throughput', 3)
         # Visualize again
         VisualizeThroughputLinearData(df_node, node)
+
+# CleanThroughputData is a function to do all cleaning functions in one swoop.
+# It accepts an excel file and will load the content into dataframe. 
+# It returns a dict of cleaned throughput dataframe for all nodes
+def CleanThroughputData(file_excel):
+    # Load the excel file
+    df = LoadDFThroughputFromExcel(file_excel)
+    # Create a df_dict {'NodeName': df_node}
+    df_dict = {}
+    # Loop through each unique node name
+    for node in list(df['Device'].unique()):
+        # Subset each device into a dataframe
+        df_node = SubsetDFThroughput(df, node)
+        # Use IQD method to transform the data, get the min and max logarithmic
+        df_node, min_log_df, max_log_df = TransformIQD(df_node)
+        # Remove outliers from dataframe
+        df_node = RemoveOutliers(df_node)
+        # Use MSD method to transform the data
+        df_node = TransformMSD(df_node, 'Throughput', 3)
+        # Assign to df_dict
+        df_dict[node] = df_node
+    return df_dict
+
+# ConstructThroughputTable is a function which accepts a dictionary of cleaned dataframe
+# and a dictionary of nodes:upper_limit_capacity. It uses both arguments and will return
+# a dataframe of summarized throughput utilization
+def ConstructThroughputTable(df_dict, node_tput_cap_dict):
+    throughput_table = pd.DataFrame(index=sorted(list(df_dict.keys())),
+        columns = ['Peak (Mbps)', 'Average (Mbps)', 'Busiest Day'])
+    tput_cap_table = pd.DataFrame(node_tput_cap_dict.items(), columns=['Device', 'Capacity (Mbps)'])
+    tput_cap_table.set_index('Device', drop=True, inplace=True)
+    throughput_table.index.name = 'Device'
+    for node, df in df_dict.items():
+        # locate the highest throughput in the data for each node, assign to Peak (Mbps)
+        throughput_table.loc[node, 'Peak (Mbps)'] = int(df['Throughput'].max() * 1000)
+        # calculate the average throughput in the data for each node, assign to Average (Mbps)
+        throughput_table.loc[node, 'Average (Mbps)'] = int(df['Throughput'].mean() * 1000)
+        # locate the date where it has the highest throughput
+        throughput_table.loc[node, 'Busiest Day'] = df['Throughput'].idxmax(axis=0).date() 
+    # Concat the throughput and node throughput cap dataframe
+    df = pd.concat([throughput_table, tput_cap_table], axis=1)
+    # Append a totals row
+    df = df.append(pd.DataFrame({\
+            'Capacity (Mbps)':tput_cap_table.loc[:,'Capacity (Mbps)'].sum(),\
+            'Peak (Mbps)':throughput_table.loc[:,'Peak (Mbps)'].sum(),\
+            'Average (Mbps)':throughput_table.loc[:,'Average (Mbps)'].sum()},index=['TOTAL']))
+    # Calculate the peak percentage against capacity
+    df['Peak Utilization (%)'] = (df['Peak (Mbps)'] / df['Capacity (Mbps)']) * 100
+    df['Peak Utilization (%)'] = df['Peak Utilization (%)'].apply(lambda x: np.round(x, decimals=2))
+    # Calculate the mean percentage against capacity
+    df['Mean Utilization (%)'] = (df['Average (Mbps)'] / df['Capacity (Mbps)']) * 100
+    df['Mean Utilization (%)'] = df['Mean Utilization (%)'].apply(lambda x: np.round(x, decimals=2))
+    # Handle NaN in Busiest Day: change to blank
+    df = df.fillna('')
+    return df
 
 # Define a map of committed throughput capacity for each GGSN
 tputcap = {'GGBNB01':66000,
